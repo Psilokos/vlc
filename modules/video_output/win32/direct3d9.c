d12976cd465739db8da384fc2bbc4a1e0ee75f8f
Victorien Le Couviour--Tuffet <victorien.lecouviour.tuffet@gmail.com>
2017-05-18 15:17:54 +0200

    vout: d3d9: resize format to the maximum size the HW can handle (if bigger)
    This fixes the issue #18215 for Windows when using D3D9 vout.

--- modules/video_output/win32/direct3d9.c
+++ modules/video_output/win32/direct3d9.c
@@ -688,6 +688,45 @@ static HINSTANCE Direct3D9LoadShaderLibrary(void)
     return instance;
 }
 
+static void
+Direct3D9ResizeFormatToMaxTextureSize(video_format_t *fmt,
+                                      DWORD max_tex_width,
+                                      DWORD max_tex_height)
+{
+    if (fmt->i_width > fmt->i_height)
+        goto resize_from_width;
+    else
+        goto resize_from_height;
+
+resize_from_width:
+    unsigned int const  vis_w = fmt->i_visible_width;
+    unsigned int const  vis_h = fmt->i_visible_height;
+    unsigned int const  nw_w = max_tex_width;
+    unsigned int const  nw_vis_w = nw_w * vis_w / fmt->i_width;
+
+    fmt->i_height = nw_w * fmt->i_height / fmt->i_width;
+    fmt->i_width = nw_w;
+    fmt->i_visible_height = nw_vis_w * vis_h / vis_w;
+    fmt->i_visible_width = nw_vis_w;
+    if (fmt->i_height > max_tex_height)
+        goto resize_from_height;
+    return;
+
+resize_from_height:
+    unsigned int const  vis_w = fmt->i_visible_width;
+    unsigned int const  vis_h = fmt->i_visible_height;
+    unsigned int const  nw_h = max_tex_size;
+    unsigned int const  nw_vis_h = nw_h * vis_h / fmt->i_height;
+
+    fmt->i_width = nw_h * fmt->i_width / fmt->i_height;
+    fmt->i_height = nw_h;
+    fmt->i_visible_width = nw_vis_h * vis_w / vis_h;
+    fmt->i_visible_height = nw_vis_h;
+    if (fmt->i_width > max_tex_width)
+        goto resize_from_width;
+    return;
+}
+
 /**
  * It initializes an instance of Direct3D9
  */
@@ -756,13 +795,9 @@ static int Direct3D9Create(vout_display_t *vd)
 
     if ( vd->fmt.i_width > sys->d3dcaps.MaxTextureWidth ||
          vd->fmt.i_height > sys->d3dcaps.MaxTextureHeight )
-    {
-        msg_Err(vd, "Textures too large %ux%u max possible: %ux%u",
-                vd->fmt.i_width, vd->fmt.i_height,
-                (unsigned) sys->d3dcaps.MaxTextureWidth,
-                (unsigned) sys->d3dcaps.MaxTextureHeight);
-        return VLC_EGENERIC;
-    }
+        Direct3D9ResizeFormatToMaxTextureSize(&vd->fmt,
+                                              sys->d3dcaps.MaxTextureWidth,
+                                              sys->d3dcaps.MaxTextureHeight);
 
     return VLC_SUCCESS;
 }
