1165c35da3385be1e37c46d4dabf316289379741
Victorien Le Couviour--Tuffet <victorien.lecouviour.tuffet@gmail.com>
2017-07-11 17:41:55 +0200

    Revert "videotoolbox: force decoder output to nv12"
    This reverts commit 99fee68f73c117dcde0adc317e188d573a9366cb.

--- modules/codec/vt_utils.c
+++ modules/codec/vt_utils.c
@@ -198,9 +198,13 @@ cvpxpool_create(const video_format_t *fmt, unsigned count)
     switch (fmt->i_chroma)
     {
         case VLC_CODEC_CVPX_UYVY:
+            cvpx_format = kCVPixelFormatType_422YpCbCr8;
+            break;
         case VLC_CODEC_CVPX_NV12:
-        case VLC_CODEC_CVPX_I420:
             cvpx_format = kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange;
+            break;
+        case VLC_CODEC_CVPX_I420:
+            cvpx_format = kCVPixelFormatType_420YpCbCr8Planar;
             break;
         case VLC_CODEC_CVPX_BGRA:
             cvpx_format = kCVPixelFormatType_32BGRA;
