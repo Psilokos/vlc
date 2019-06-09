44ae6afec26baef6441e0c2db16642117c228237
Victorien Le Couviour--Tuffet <victorien@videolan.org>
2019-06-09 21:36:56 +0200

    packetizer: add startcode_FindAnnexB SSSE3 asm

--- modules/packetizer/startcode.asm
+++ modules/packetizer/startcode.asm
@@ -22,7 +22,10 @@
 
 SECTION_RODATA 16
 
-pd_0x01000000: times 4 dd 0x01000000
+shuf_pad2dw: db 0x00, 0x01, 0x02, 0x80, 0x03, 0x04, 0x05, 0x80
+             db 0x06, 0x07, 0x08, 0x80, 0x09, 0x0A, 0x0B, 0x80
+
+pd_0x00010000: times 4 dd 0x00010000
 
 SECTION .text
 
@@ -33,8 +36,9 @@ cglobal startcode_FindAnnexB, 2, 7, 6, ptr, end, size
     cmp          sized, 16
     jl .end
 
-    LEA            r6q, pd_0x01000000
+    LEA            r6q, pd_0x00010000
     mova            m0, [r6q]
+    pslld           m0, 8
 .loop:
     movu            m1, [ptrq]
     psrldq          m2, m1, 1
@@ -91,5 +95,79 @@ DEFINE_ARGS ptr, _, size, tmp
     RET
 
 .found:
+    lea            rax, [ptrq+r3q]
+    RET
+
+INIT_XMM ssse3
+cglobal startcode_FindAnnexB, 2, 7, 5, ptr, end, size
+%define base r6q-shuf_pad2dw
+    mov          sizeq, endq
+    sub          sizeq, ptrq
+    cmp          sized, 16
+    jl .end
+
+    LEA            r6q, shuf_pad2dw
+    mova            m0, [base+shuf_pad2dw]
+    mova            m1, [base+pd_0x00010000]
+.loop:
+    movu            m2, [ptrq]
+    psrldq          m3, m2, 1
+    psrldq          m4, m2, 2
+    pshufb          m2, m0
+    pshufb          m3, m0
+    pshufb          m4, m0
+    pcmpeqd         m2, m1
+    pcmpeqd         m3, m1
+    pcmpeqd         m4, m1
+    movmskps       r3d, m2
+    movmskps       r4d, m3
+    movmskps       r5d, m4
+    tzcnt          r3d, r3d
+    tzcnt          r4d, r4d
+    tzcnt          r5d, r5d
+    cmp            r4d, r3d
+    cmovle         r3d, r4d
+    setle          r4b
+    cmp            r5d, r3d
+    cmovle         r3d, r5d
+    setle          r5b
+    test           r3d, 32
+    jz .found
+    add           ptrq, 12
+    sub          sized, 12
+    cmp          sized, 16
+    jge .loop
+
+DEFINE_ARGS ptr, _, size, tmp
+.end:
+    sub          sized, 3
+    jl .ret_null
+.end_loop:
+    xor           tmpd, tmpd
+    test   word [ptrq], 0xFFFF
+    cmovz         tmpw, [ptrq+1]
+    xor           tmpd, 0x0100
+    jz .ret
+    inc           ptrq
+    dec          sized
+    jge .end_loop
+.ret_null:
+    xor           ptrq, ptrq
+.ret:
+%if ARCH_X86_32
+    mov            eax, ptrd
+%else
+    mov            rax, ptrq
+%endif
+    RET
+
+.found:
+    lea            r3d, [r3d*3]
+    mov            r6d, r4d
+    xor            r4d, r5d
+    and            r4d, r6d
+    shl            r5d, 1
+    or             r4d, r5d
+    add            r3d, r4d
     lea            rax, [ptrq+r3q]
     RET
